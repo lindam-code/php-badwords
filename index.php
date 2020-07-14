ee9bebbd2d7b9d5a0d8ee85567bf338932d0e24b
lindam <linda.minotti2@gmail.com>
2020-07-14 15:07:27 +0200

    Refactoring.

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
   reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
   Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
   mollit anim id est laborum.';
+  // Sostituisco la parola da cambiare con tre asterischi
   $testo_badword = str_replace($_GET["badword"], '***', $testo);
 ?>
-
-<h1>Testo</h1>
-<p><?php echo $testo ?></p>
-<span> <?php echo ' Lunghezza del lorem ipsum : ' . strlen($testo) . ' caratteri.' ?></span>
-<h2>Testo con le badwords</h2>
-<p><?php echo $testo_badword ?></p>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>badword</title>
+  </head>
+  <body>
+    <h1>Testo</h1>
+    <p><?php echo $testo ?></p>
+    <span>Lunghezza del lorem ipsum : <?php echo strlen($testo); ?> caratteri.</span>
+    <h2>Testo censurato</h2>
+    <p><?php echo $testo_badword ?></p>
+    <span>Lunghezza del lorem ipsum censurato : <?php echo strlen($testo_badword); ?> caratteri.</span>
+  </body>
+</html>
